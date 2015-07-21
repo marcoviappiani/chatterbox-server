54bb81bb59b478900f0dcf7be07f63ea4b5df272
marcoviappiani <marco.viappiani@gmail.com>
2015-07-20 19:23:48 -0700

    Passed all spec tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,102 +12,60 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var results = [];
+var validUrls = ['/classes/messages','/classes/room1',];
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+
+exports.requestHandler = function(request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  // The outgoing status.
   var statusCode;
   var jsonObject = {
     message: "",
-    results: []
+    results: results
   }; 
-  // See the note below about CORS headers.
+ 
   var headers = defaultCorsHeaders;
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
+ 
   headers['Content-Type'] = "application/JSON";
   
-  if(request.url === undefined) {
+  if(validUrls.indexOf(request.url) === -1) {
     statusCode = 404;
     response.writeHead(statusCode, headers);
     response.end(JSON.stringify(jsonObject));
-  }
-
-  if(request.method === 'GET') {
+    
+  } else if(request.method === 'GET') {
     statusCode = 200;
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-
+    response.writeHead(statusCode, headers);
     jsonObject.message = "Hello, World!";
     response.end(JSON.stringify(jsonObject));
-  }
 
-  if(request.method === 'POST'){
+  } else if(request.method === 'POST'){
     statusCode = 201;
     response.writeHead(statusCode, headers);
     var body;
     body = '';
-      // we want to get the data as utf8 strings
-      // If you don't set an encoding, then you'll get Buffer objects
-      request.setEncoding('utf8');
+    request.setEncoding('utf8');
 
-      // Readable streams emit 'data' events once a listener is added
-      request.on('data', function (chunk) {
-        body += chunk;
-        // console.log(body);
-      });
+    request.on('data', function (chunk) {
+      body += chunk;
+    });
 
-      // the end event tells you that you have entire body
-      request.on('end', function () {
-        try {
-          var data = JSON.parse(body);
-          jsonObject.results.push(data);
-          // console.log(JSON.parse(body).results);
-          // console.log(data);
-        } catch (er) {
-          // uh oh!  bad json!
-          response.statusCode = 400;   // this is probably wrong - need to fix it at some point
-          return response.end('error: ' + er.message);
-        }
+    request.on('end', function () {
+      try {
+        var data = JSON.parse(body);
+        results.push(data);
+      } catch (er) {
+        response.statusCode = 400;   // this is probably wrong - need to fix it at some point
+        return response.end('error: ' + er.message);
+      }
+      response.end(JSON.stringify(jsonObject));
+    });
 
-        // write back something interesting to the user:
-        console.log(response);
-        response.write(typeof data);
-        response.end(JSON.stringify(jsonObject));
-      });
   }
 };
 
 
-// exports.send = function () {
-
-// };
 
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
